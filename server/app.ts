89153d993553a40ffd8203615232f541633ac921
davitvoski <davitvoskerchyan@outlook.com>
2023-01-17 19:31:20 -0500

    Add google authentication

--- server/app.ts
+++ server/app.ts
@@ -2,35 +2,44 @@ import dotenv from 'dotenv'
 import express from 'express'
 import compression from 'compression'
 import { auth, ConfigParams } from 'express-openid-connect'
+import passport from "passport"
+import { addGooglePassportStrategy } from './auth/passport_google_oidc'
+import session from 'express-session'
+
+
 dotenv.config()
 
+
 const app = express()
-
-// AUTH0 CONFIGURATION
-const authConfig: ConfigParams = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env['SECRET'] as string,
-    baseURL: 'http://localhost:3000', // TODO: Change this to the URL of your app in .env
-    clientID: process.env['CLIENT_ID'] as string,
-    issuerBaseURL: process.env["ISSUER_BASE_URL"] as string
-}
-app.use(auth(authConfig))
-
 // Expreess middleware
+let secret: {
+    secret: string,
+    resave: boolean,
+    saveUninitialized: boolean
+} = { secret: process.env['SECRET'] as string, resave: false, saveUninitialized: true }
+app.use(session(
+    secret
+))
+
 app.use(compression())
 app.use(express.json())
 app.use(express.static("../client/build"))
 
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+addGooglePassportStrategy()
 
-// app.get('/login', (req: express.Request, res: express.Response) => {
-//     res.send('LOGIN PAGE')
-// })
+app.get('/login/google', passport.authenticate('google'))
 
+app.get('/oauth2/redirect',
+    passport.authenticate('google', { failureRedirect: '/login', failureMessage: true }),
+    function (req, res) {
+        res.redirect('/');
+    }
+)
 
+app.get('/login', (_: express.Request, res: express.Response) => {
+    res.send('login')
+
+})
 // Default 404
 app.use((_: express.Request, res: express.Response) => {
     res.sendStatus(404)
