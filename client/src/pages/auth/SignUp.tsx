d07e6811b248adff73d0dc3ee0f2e7373ff2273b
davitvoski <davitvoskerchyan@outlook.com>
2023-01-29 20:07:32 -0500

    Add Signup Functioality - Works

---
+++ client/src/pages/auth/SignUp.tsx
@@ -0,0 +1,86 @@
+import { Input, TextField } from "@mui/joy";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+export default function SignUp() {
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [isErrorFields, setErrorFields] = useState(false);
+    const [errorFailed, setErrorFailed] = useState("");
+
+
+    function checkEmail() {
+        const emailRegex = new RegExp('^[a-zA-Z|0-9]+@[a-zA-Z]+.[com|ca]+$')
+        if (emailRegex.test(email as string)) {
+            console.log("email paassed")
+            return true
+        } else {
+            console.log("email failed")
+            return false
+        }
+    }
+
+    async function fetchData(e: FormEvent) {
+        e.preventDefault()
+
+        // TODO: find a way to reset the error state
+        setErrorFields(false)
+
+        if (username === "" || password === "" || checkEmail() !== true) {
+            setErrorFields(true)
+            return
+        }
+
+        await axios.post('api/signup', {
+            username: username,
+            password: password,
+            email: email
+        }, {
+            // validateStatus: (status) => {
+            //     return status === 200
+            // },
+            headers: {
+                "content-type": 'application/json'
+            }
+        }).then((res: AxiosResponse) => {
+            // TODO: Set the user Context
+            navigate('/profile')
+
+        }).catch((err: any) => {
+            if (!err.response.data.available) {
+                setErrorFailed(err.response.data!!.error)
+            } else {
+                setErrorFailed("Could not sign up. Please try again later.")
+
+            }
+        })
+    }
+
+    return (
+        <>
+            <div className="flex gap-4 -translate-y-10 flex-col justify-center items-center text-center w-full h-full">
+                <h1 className="text-2xl">SIGN UP</h1>
+                {isErrorFields && <h1 className='text-sm text-error'>Please Enter All Fields And Try Again</h1>}
+
+                <form id="signup-form" className="flex flex-col gap-4">
+                    <Input className="w-5/5" name="signup-form" onChange={(e) => setUsername(e.target.value)} placeholder="Enter Your Username" type="text" variant="outlined" required />
+                    <Input className="w-5/5" name="signup-form" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Your Password" type="password" variant="outlined" required />
+                    <Input className="w-5/5" name="signup-form" onChange={(e) => setEmail(e.target.value)} placeholder="Enter A .com or .ca Email" type="email" variant="outlined" required />
+
+                    <h5 className="text-xs text-start text-sky-200 transition ease-in-out delay-100 hover:text-action">
+                        <Link to="/login">Already have an account? Click Me</Link>
+                    </h5>
+                    {errorFailed && <h1 className='text-sm text-error '>{errorFailed}</h1>}
+
+                    <button type='submit' onClick={fetchData} form='signup-form' className="bg-sky-500 p-2 rounded-xl w-1/2 self-center transition ease-in-out delay-100 hover:bg-action ">
+                        SIGN UP</button>
+                </form>
+            </div>
+        </>
+    )
+}
--- server/routes/authentication.routes.ts
+++ server/routes/authentication.routes.ts
@@ -2,7 +2,10 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt'
 import session from "express-session";
+import { generateHash } from "../utils/generateHash";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
+// TODO: Add google authentication
 const router = express.Router()
 const prisma = new PrismaClient()
 const saltRounds = 10
@@ -18,6 +21,35 @@ router.post("/logout", (req: express.Request, res: express.Response) => {
     res.redirect('/login')
 })
 
+router.post("/signup", async (req: express.Request, res: express.Response) => {
+    try {
+
+        const [salt, hashedPassword] = generateHash(req.body.password)
+
+        const user = await prisma.user.create({
+            data: {
+                email: req.body.email,
+                userName: req.body.username,
+                password: hashedPassword
+            }
+        })
+        console.log(user)
+
+        res.status(201).send({ user: user })
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+        // Check if user already exists
+        if (err instanceof PrismaClientKnownRequestError) {
+            res.json({ available: false, error: "Username Taken - Please Choose Another Name." })
+        } else {
+            res.send()
+        }
+    }
+
+})
+
+
 router.post('/login', async (req: express.Request, res: express.Response) => {
     if (USER_SESSION?.userid) {
         // TODO: Redirect to already logged in page
@@ -26,9 +58,8 @@ router.post('/login', async (req: express.Request, res: express.Response) => {
     }
 
     try {
-        console.log('req', req.body)
         const salt = bcrypt.genSaltSync(saltRounds)
-        const hash = await bcrypt.hash(req.body.password, salt)
+        const hash = bcrypt.hashSync(req.body.password, salt)
 
         const user = (await prisma.user.findMany({
             where: {
@@ -37,8 +68,11 @@ router.post('/login', async (req: express.Request, res: express.Response) => {
             }
         }))[0]
 
+        if (!user)
+            throw Error("User not found")
+
         if (user.password != hash)
-            res.sendStatus(401)
+            throw Error("Password incorrect")
 
         type UserWithoutPassword = Omit<typeof user, "password">
         USER_SESSION = req.session
