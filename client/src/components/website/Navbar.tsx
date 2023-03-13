43abb1a6a1ab672fe98731ff3f67ea0ff2c07d69
davitvoski <davitvoskerchyan@outlook.com>
2023-03-13 16:05:15 -0400

    ADd profile

--- client/src/components/website/Navbar.tsx
+++ client/src/components/website/Navbar.tsx
@@ -4,42 +4,47 @@ export default function Navbar() {
   const user = "";
   return (
     <>
-      <nav className="h-full w-full  border-gray-200 bg-white px-2 dark:bg-gray-900">
+      <nav className="h-full w-full border-gray-200 bg-white px-2 dark:bg-gray-900">
         <div className="container mx-auto flex h-full flex-wrap items-center justify-between">
-          <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:bg-action">
-            dsadas
-            <Link to="/" className="inline-block hover:cursor-pointer ">
-              CHAT
-            </Link>
-          </span>
-          <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:text-action">
-            <Link to="/" className="inline-block hover:cursor-pointer ">
-              INVENTORY
-            </Link>
-          </span>
-          {/* <div className="ml-4 flex gap-4 p-2">
-            <span className="p-5 hover:cursor-pointer hover:bg-orange-400 ">
-              dsadasdsa
-              {/* <li className="list-none border-2 border-purple p-5 shadow-md  shadow-action hover:bg-orange-400">
-                dsadasdaasd
-              </li> */}
-          {/* </span> */}
-
-          {/* <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:text-action">
+          <div className="flex justify-evenly gap-8">
+            <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:bg-action">
               <Link to="/" className="inline-block hover:cursor-pointer ">
-                CHAT
+                Home
               </Link>
             </span>
-            <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:text-action">
-              <Link to="/" className="inline-block hover:cursor-pointer ">
-                INVENTORY
+            <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:bg-action">
+              <Link
+                to="/inventory"
+                className="inline-block hover:cursor-pointer "
+              >
+                Inventory
               </Link>
-            </span> */}
-          {/* </div> */}
-          <div>
+            </span>
+            <span className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:bg-action">
+              <Link to="/" className="inline-block hover:cursor-pointer ">
+                Chat
+              </Link>
+            </span>
+          </div>
+          <div className="flex gap-6">
+            {user && (
+              <li className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:text-action">
+                <Link
+                  to="/profile"
+                  state={{ user_created: "Logout Sucessfully" }}
+                >
+                  Profile
+                </Link>
+              </li>
+            )}
             <li className="list-none border-2 border-purple p-2 shadow-md  shadow-action hover:text-action">
               {user ? (
-                <Link to="/logout">Logout</Link>
+                <Link
+                  to="/logout"
+                  state={{ user_created: "Logout Sucessfully" }}
+                >
+                  Logout
+                </Link>
               ) : (
                 <Link to="/login">Login</Link>
               )}
