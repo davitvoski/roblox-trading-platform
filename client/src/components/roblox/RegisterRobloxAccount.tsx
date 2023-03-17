2a3aa45c0df1800e3448f1f72a24ef95cd4a3625
davitvoski <davitvoskerchyan@outlook.com>
2023-03-16 23:40:58 -0400

    Add UI to get robloc

--- client/src/components/roblox/RegisterRobloxAccount.tsx
+++ client/src/components/roblox/RegisterRobloxAccount.tsx
@@ -1,7 +1,49 @@
+import { Input } from "@mui/joy";
+import axios from "axios";
+import { FormEvent } from "react";
+
 export default function RegisterRobloxAccount() {
+  async function checkAccount(e: FormEvent) {
+    e.preventDefault();
+    const username = (e.target as HTMLFormElement).username.value as string;
+
+    await axios
+      .get(`/api/roblox/users/${username}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  }
+
   return (
-    <div>
-      <h1>Register Roblox Account</h1>
-    </div>
+    <>
+      <section className="0 m-5 flex w-full items-center justify-center">
+        <section className="-mt-40 gap-5">
+          <form
+            id="roblox-form"
+            className="flex flex-col items-center justify-center gap-4"
+            onSubmit={checkAccount}
+          >
+            <h1 className="w-fit text-3xl">Let's Register </h1>
+            <Input
+              className="w-fit"
+              name="username"
+              placeholder="Enter Your Roblox Username"
+              type="text"
+              variant="outlined"
+            />
+            <button
+              type="submit"
+              form="roblox-form"
+              className="w-1/2 self-center rounded-xl bg-sky-500 p-2 transition delay-100 ease-in-out hover:bg-action "
+            >
+              Link Account
+            </button>
+          </form>
+        </section>
+      </section>
+    </>
   );
 }
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,9 @@
+import RegisterRobloxAccount from "../components/roblox/RegisterRobloxAccount";
+
 export default function Profile() {
   return (
     <section className="mt-52 flex h-full w-full flex-col md:mt-0 md:flex-row">
-      <section className=" -mt-40 flex h-fit w-1/3 min-w-min flex-col items-center justify-center self-center ">
+      <section className="-mt-40 flex h-fit w-1/3 min-w-min flex-col items-center justify-center self-center ">
         <div className="h-52 w-52">
           <img
             className="h-full w-full rounded-full border border-gray-100 shadow-sm"
@@ -11,7 +13,7 @@ export default function Profile() {
         </div>
         <h1 className=" w-fit text-5xl">davitvoski</h1>
       </section>
-      <section className="m-5 w-2/3 border border-green-400"></section>
+      <RegisterRobloxAccount />
     </section>
   );
   // https://users.roblox.com/v1/users/48176515
