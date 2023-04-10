a13646a60fa1e77edbcc8390fc2fc30c91c1b133
davitvoski <davitvoskerchyan@outlook.com>
2023-04-10 13:36:29 -0400

    Add next basic project

--- shared/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare interface PubUser {
-    userName: string
-    email: string
-    avatar?: Blob
-    robloxId: number = null
-    robloxUsername: string = null
-    robloxUser: RobloxUser = null
-}
